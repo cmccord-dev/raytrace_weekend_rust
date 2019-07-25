4908babd1362b0e873ee8149036949bc7a120677
eiowlta <cassandra.l.mccord@gmail.com>
2019-07-25 16:52:54 +0300

    cleaned up a bit

--- src/raytracer.rs
+++ src/raytracer.rs
@@ -29,31 +29,15 @@ pub fn raytrace(image: &mut Image) {
         }
     }
 }
-fn hit_sphere(center: &Vec3, radius: f32, ray: &Ray) -> f32 {
-    let oc = ray.src - center;
-    let a = ray.dir.dot(ray.dir);
-    let b = 2.0 * oc.dot(ray.dir);
-    let c = oc.dot(oc) - radius * radius;
-    let discr = b * b - 4.0 * a * c;
-    if discr < 0.0 {
-        -1.0
-    } else {
-        (-b - discr.sqrt()) / (2.0 * a)
-    }
-}
-fn getHit(ray: &Ray, objs: &Vec<Box<Object>>) -> Option<Hit> {
+fn get_hit(ray: &Ray, objs: &Vec<Box<Object>>) -> Option<Hit> {
     objs.iter()
         .map(|obj| (*obj).hits(ray, 0.0, f32::MAX))
         .filter_map(|hit| hit)
         .min_by(|a, b| a.t.partial_cmp(&b.t).unwrap_or(std::cmp::Ordering::Equal))
 }
 fn color(ray: &Ray, objs: &Vec<Box<Object>>) -> Vec3 {
-    if let Some(tmp) = getHit(ray, objs) {
+    if let Some(tmp) = get_hit(ray, objs) {
         (tmp.normal + 1.0) * 0.5
-
-    /*let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
-    if t > 0.0 {
-        ((ray.at_parameter(t) - Vec3::new(0.0, 0.0, -1.0)).norm()+1.0)*0.5*/
     } else {
         let t: f32 = 0.5 * (ray.dir.y + 1.0);
         Vec3::lerp(Vec3::from_value(1.0), Vec3::new(0.5, 0.7, 1.0), t)
